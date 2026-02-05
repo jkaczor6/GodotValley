c49aecf656d6c39570f8747e47681134f3515115
Jakub Kaczor <j.kaczor06@gmail.com>
2026-02-05 18:07:27 +0100

    finished character animations

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -2,10 +2,16 @@ extends CharacterBody2D
 
 var direction: Vector2
 var speed := 50
+@onready var move_state_machine = $Animation/AnimationTree.get("parameters/MoveStateMachine/playback")
 
 func _physics_process(_delta: float) -> void:
+	get_basic_input()
 	move()
 	animate()
+
+func get_basic_input():
+	if Input.is_action_just_pressed("action"):
+		$Animation/AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 func move():
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -14,10 +20,15 @@ func move():
 
 func animate():
 	if direction:
+		move_state_machine.travel('Walk')
 		var direction_animation = Vector2(round(direction.x), round(direction.y))
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
 		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", direction_animation)
+		for animation in Data.TOOL_STATE_ANIMATIONS.values():
+			var animation_name : String = "parameters/ToolStateMachine/"+ animation +"/blend_position"
+			$Animation/AnimationTree.set(animation_name, direction_animation)
 	else:
-		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.DOWN)
+		move_state_machine.travel('Idle')
 
 func tool_use_emit():
 	print('tool')
