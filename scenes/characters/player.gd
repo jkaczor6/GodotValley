544c1e22a3a1a6a5ba100827c89db18d121730c7
Jakub Kaczor <j.kaczor06@gmail.com>
2026-02-05 17:48:56 +0100

    added animations for walking

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,4 +1,23 @@
 extends CharacterBody2D
 
+var direction: Vector2
+var speed := 50
+
+func _physics_process(_delta: float) -> void:
+	move()
+	animate()
+
+func move():
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
+
+func animate():
+	if direction:
+		var direction_animation = Vector2(round(direction.x), round(direction.y))
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", direction_animation)
+	else:
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.DOWN)
+
 func tool_use_emit():
 	print('tool')
