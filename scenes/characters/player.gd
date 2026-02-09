e082f18437733e718f957683ba76a1876bfaceeb
Jakub Kaczor <j.kaczor06@gmail.com>
2026-02-09 17:00:09 +0100

    added styles for player, started implementing machines

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -9,6 +9,7 @@ var can_move : bool = true
 var current_tool: Enum.Tool = Enum.Tool.AXE
 var current_seed: Enum.Seed
 var current_state: Enum.State
+var current_style: Enum.Style
 
 signal tool_use(tool: Enum.Tool, pos: Vector2)
 signal diagnose
@@ -52,6 +53,10 @@ func get_basic_input():
 	
 	if Input.is_action_just_pressed("diagnose"):
 		diagnose.emit()
+	
+	if Input.is_action_just_pressed("style_toggle"):
+		current_style = posmod(current_style + 1, Enum.Style.size()) as Enum.Style
+		$Sprite2D.texture = Data.PLAYER_SKINS[current_style]
 
 func move():
 	direction = Input.get_vector("left", "right", "up", "down")
